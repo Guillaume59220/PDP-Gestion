8351f4aee9e457f3d3229276e181fefbcba20af7
Your Name <guillaumedu59220@hotmail.com>
2018-01-04 13:50:15 +0100

    commit de 13:50

--- Menu/index.php
+++ Menu/index.php
@@ -4,10 +4,42 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <title>Menu</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
 </head>
 <body>
-    <div class="">
+
+    <div class="menu">
+
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-8">
+                
+                <div id="image">
+                
+                    <img src="img/icone applications/courrier/bureaupdp.png" alt="">
+            
+                </div>
+
+            </div>
+
+            <div class="col-xs-6 col-md-4">
+                
+                <div id="boutons">
+                    <img src="img/icone applications/courrier/telephonie.png" alt="">
+                    <img src="img/icone applications/courrier/courrier.png" alt="">
+                    <img src="img/icone applications/courrier/domiciliation.png" alt="">
+                </div>
+
+            </div>
+
+        </div>
 
     </div>
 </body>
