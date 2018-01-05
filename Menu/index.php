53c9d56fc1728593b9d4e5ed7def0d3a60d39306
Your Name <guillaumedu59220@hotmail.com>
2018-01-05 10:24:36 +0100

    commit du 05/01/2018 à 10:24

--- Menu/index.php
+++ Menu/index.php
@@ -1,57 +1,72 @@
 <!DOCTYPE html>
 <html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-    <title>Menu</title>
+        <link rel="stylesheet" href="css/style.css">
+        
+        <title>Menu</title>
 
-    <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
 
-</head>
-<body class="container">
+        <div class="menu">
 
-    <div class="menu">
-
-        <div class="row">
-            <div class="col-xs-12 col-sm-6 col-md-8">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-8">
+                    
+                    <div id="image">
+                    
+                        <img src="img/icone_applications/courrier/bureaupdp.png" alt="">
                 
-                <div id="image">
-                
-                    <img src="img/icone_applications/courrier/bureaupdp.png" alt="">
-            
+                    </div>
+
                 </div>
 
-            </div>
+                <div class="col-xs-6 col-md-4">
+                    
+                    <div id="boutons">
 
-            <div class="col-xs-6 col-md-4">
-                
-                <div id="boutons">
-                    <img id="telephonie" src="img/icone_applications/courrier/telephonie.png" alt="Téléphonie">
-                    
-                    <img id="courrier" src="img/icone_applications/courrier/courrier.png" alt="Courrier">
-                    
-                    <img id="domiciliation" src="img/icone_applications/courrier/domiciliation.png" alt="Domiciliation">
-                    
-                    <img id="paie" src="img/icone_applications/courrier/paie.png" alt="Paie">
-                    
-                    <img id="gestion" src="img/icone_applications/courrier/gestion.png" alt="Gestion">
-                    
-                    <img id="recouvrement" src="img/icone_applications/courrier/recouvrement.png" alt="Recouvrement">
-                    
-                    <img id="audit" src="img/icone_applications/courrier/audit.png" alt="Audit">
-                
+                        <div id="telephonie">
+                        <a href="http://"><img src="img/icone_applications/courrier/telephonie.png" alt="Téléphonie"></a>
+                        </div>
+
+                        <div id="courrier">
+                            <a href="http://"><img src="img/icone_applications/courrier/courrier.png" alt="Courrier"></a>
+                        </div>
+
+                        <div id="domiciliation">
+                            <a href="http://"><img src="img/icone_applications/courrier/domiciliation.png" alt="Domiciliation"></a>
+                        </div>
+
+                        <div id="paie">
+                            <a href="http://"><img src="img/icone_applications/courrier/paie.png" alt="Paie"></a>
+                        </div>
+
+                        <div id="gestion">
+                            <a href="http://"><img src="img/icone_applications/courrier/gestion.png" alt="Gestion"></a>
+                        </div>
+
+                        <div id="recouvrement">
+                            <a href="http://"><img src="img/icone_applications/courrier/recouvrement.png" alt="Recouvrement"></a>
+                        </div>
+
+                        <div  id="audit">
+                            <a href="http://"><img src="img/icone_applications/courrier/audit.png" alt="Audit"></a>                    
+                        </div>
+
+                    </div>
+
                 </div>
 
             </div>
 
         </div>
-
-    </div>
-</body>
+    </body>
 </html>
