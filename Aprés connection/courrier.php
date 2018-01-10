43157f6a9afa8903cfa36161af4f91dc2c5e1bd2
Your Name <guillaumedu59220@hotmail.com>
2018-01-10 16:37:12 +0100

    commit de 16:37

--- Aprés connection/courrier.php
+++ Aprés connection/courrier.php
@@ -156,17 +156,7 @@
                               <div class="col-xs-12">
                                   <div class="col-md-12 well text-center">
                       
-                            <label for="fileToUpload">Upload du fichier scan</label><br />
-                            <input type="file" name="fileToUpload" id="fileToUpload" onchange="fileSelected();"/>
-                          </div>
-                          <div id="fileName"></div>
-                          <div id="fileSize"></div>
-                          <div id="fileType"></div>
-                          <div class="row">
-                            <input type="button" onclick="uploadFile()" value="Upload" />
-                          </div>
-                          <div id="progressNumber"></div>
-                        </form>
+                            
 
                         <br>
 
--- Aprés connection/scan_detail.php
+++ Aprés connection/scan_detail.php
@@ -1,17 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+        <meta charset="utf-8">
+        
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!-- InstanceBeginEditable name="doctitle" -->
+            <title>Service clients</title>
+            <!-- InstanceEndEditable --><!-- InstanceBeginEditable name="head" -->
+            <!-- InstanceEndEditable -->
+            <!-- Menu tab -->
+        
+            <script src="Spry-master/includes_packed/SpryTabbedPanels.js" type="text/javascript"></script>
+
+            <!-- Bootstrap -->
+            <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css">
+            <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/js/bootstrap.min.js"></script>
+
+            <script src="../../js/ie10-viewport-bug-workaround.min.js"></script>
+            
+            <!--jQuery et jQuery UI-->
+            <!-- Datepicker -->
+            <script type="text/javascript" src="jquery-ui-1.12.1/jquery-ui.js"></script>
+
+            <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+
+            <link rel="stylesheet" type="text/css" href="jquery-ui.min.css ">
+
+
+            <!--FICHIERS LOCAUX-->
+            <link rel="stylesheet" type="text/css" href="CSS/style.css">
+            <script src="JS/script.js"></script>
+
+
+            <script>
+                $(function() {
+                var date = $('#datepicker').datepicker({ dateFormat: 'yy-mm-dd' }).val();
+                var date = $('#datepicker4').datepicker({ dateFormat: 'yy-mm-dd' }).val();
+                var date = $('#datepicker5').datepicker({ dateFormat: 'yy-mm-dd' }).val();
+                });
+                // Popup
+                function MM_jumpMenu(targ,selObj,restore){ // v3.0
+                  eval(targ+".location='"+selObj.options[selObj.selectedIndex].value+"'");
+                  if (restore) selObj.selectedIndex=0;
+                }
+            </script>
+            
+            <script src="http://css3-mediaqueries-js.googlecode.com/files/css3-mediaqueries.js"></script>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 </head>
 <body>
 
     <div class="scan">
 
         <div class="form">
-            <h3>  </h3>
+            <p>Veuillez trouver le scan de votre courrier ci-dessous</p>
+
+            
         </div>
 
     </div>
