ad71912550d36a3d7ff8f4488ee8deb72907aca0
KatKarwat <karwatkatarzyna@gmail.com>
2018-01-11 10:52:14 +0100

    bdd domiciliation

--- Courrier/script.js
+++ Courrier/script.js
@@ -9,20 +9,6 @@
 
          if($('#newsletter').find('.has-error').length == 0) {
 
-            //il n'y a pas d'erreur et je peux procéder à ma requète AJAX
-
-            // console.log($(this).serialize());
-
-            $.ajax({
-                type        : $(this).attr('method'),
-                url         : $(this).attr('action'),
-                data        : $(this).serialize(),
-                dataType    : 'JSON'
-            });
-        };
- 	});
- 	});
-
-
-       
-    
+            }
+        });
+    });
