5eb0dfd4a25bc65caa08dd34af21085c0a0fc517
KatKarwat <karwatkatarzyna@gmail.com>
2018-01-12 16:55:22 +0100

    17.00 12/01/18

--- Interne/Courrier/formulaireinsert.js
+++ Interne/Courrier/formulaireinsert.js
@@ -1,11 +1,2 @@
-$(function() {
 
- 	 console.log('jQuery is ready to go !');
-
- 	 $('#courrier').submit(function(e) {
-
-        // -- Bloquer la redirection du formulaire
-        e.preventDefault();
-
-    });
- 	})
+ 	
---
+++ Interne/Courrier/listecourrier.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>liste</title>
+</head>
+<body>
+<p>liste de courrier</p>
+</body>
+</html>
