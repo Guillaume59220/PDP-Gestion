b14171dbadf516068b0eb154a1602f3d247c6088
Your Name <guillaumedu59220@hotmail.com>
2018-01-16 14:45:47 +0100

    commit de 14:45

--- Externe/apres_connection/courrier/listes/liste1.php
+++ Externe/apres_connection/courrier/listes/liste1.php
@@ -22,7 +22,18 @@
 <body>
 <?php
 
+resource mysql_connect ([ string $server = ini_get("mysql.default_host") [, string $username = ini_get("mysql.default_user") [, string $password = ini_get("mysql.default_password") [, bool $new_link = false [, int $client_flags = 0 ]]]]] )
 
+$link = mysql_connect("localhost", "mysql_user", "mysql_password")
+    or die("Impossible de se connecter : " . mysql_error());
+    echo 'Connexion réussie';
+    mysql_close($link);
+
+    $link = mysql_connect('localhost', 'mysql_user', 'mysql_password');
+    $db_list = mysql_list_dbs($link);
+    while ($row = mysql_fetch_object($db_list)) {
+    echo $row->Database . "\n";
+    }
 
 
 
