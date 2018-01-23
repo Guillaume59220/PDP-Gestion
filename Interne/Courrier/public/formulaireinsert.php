e6503b6239ac8a0df29b96d341c4ecfca548e384
KatKarwat <karwatkatarzyna@gmail.com>
2018-01-23 17:58:41 +0100

    23/01 18h

--- Interne/Courrier/public/formulaireinsert.php
+++ Interne/Courrier/public/formulaireinsert.php
@@ -167,9 +167,3 @@ foreach($types as $type) {
 	
 </div>
 
-
-
-<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-</body>
-</html>
--- Interne/Courrier/public/menu.php
+++ Interne/Courrier/public/menu.php
@@ -2,35 +2,33 @@
 require 'header.php';
 require_once '../src/function.php';
 //<?php if (isset($_SESSION['id'])) :
-//<?php endif;
+
 ?>
 <menu>
-    <?php if(isset($_SESSION['auth'])==1): ?>
+
         <div class="row align-items-center">
             <div class="col-md-2 align-items-center">
-                <form action="adduser.php.php" method="post">
+                <form action="adduser.php" method="post">
                     <input type="hidden"  value="1"/>
-                    <button class="btn btn-lg fa fa-plus" >Ajouter un client</button>
+                    <button class="btn fa fa-plus" > Ajouter un client</button>
                 </form>
             </div>
-      <?php else: ;
-     endif; ?>
             <div class="col-md-2 align-items-center">
                 <form action="ajoutercourrier.php" method="post">
                     <input type="hidden"  value="1"/>
-                    <button class="btn btn-lg fa fa-envelope-o" >Ajouter un courrier</button>
+                    <button class="btn fa fa-envelope-o" > Ajouter un courrier</button>
                 </form>
             </div>
             <div class="col-md-2 align-items-center">
                 <form action="listecourrier.php" method="post">
                     <input type="hidden"  value="1"/>
-                    <button class="btn btn-lg fa fa-envelope-o" > Courrier traite</button>
+                    <button class="btn fa fa-envelope-o" > Courrier traite</button>
                 </form>
             </div>
             <div class="col-md-2 align-items-center">
                 <form action="connexion.php" method="post">
                     <input type="hidden" name="form_deconnexion" value="1"/>
-                    <button class="btn btn-lg fa fa-power-off" type="submit"> Deconnexion</button>
+                    <button class="btn fa fa-power-off" type="submit"> Deconnexion</button>
                 </form>
             </div>
         </div>
