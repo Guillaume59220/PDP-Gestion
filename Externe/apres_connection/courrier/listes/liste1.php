1c09a276a2fd60b2416ed2b9cd0d3892b325d39f
Guillaume-dev <guillaumedu59220@hotmail.com>
2018-03-19 15:58:38 +0100

    commit

--- Externe/apres_connection/courrier/listes/liste1.php
+++ Externe/apres_connection/courrier/listes/liste1.php
@@ -20,9 +20,22 @@
 
 </head>
 <body>
+
 <?php
 
-resource mysql_connect ([ string $server = ini_get("mysql.default_host") [, string $username = ini_get("mysql.default_user") [, string $password = ini_get("mysql.default_password") [, bool $new_link = false [, int $client_flags = 0 ]]]]] )
+/*resource mysql_connect (
+    [ string $server = ini_get("mysql.default_host") 
+        [, string $username = ini_get("mysql.default_user") 
+            [, string $password = ini_get("mysql.default_password") 
+                [, bool $new_link = false 
+                    [, int $client_flags = 0 ]
+                ]
+            ]
+        ]
+    ]
+)*/
+
+
 
 $link = mysql_connect("localhost", "mysql_user", "mysql_password")
     or die("Impossible de se connecter : " . mysql_error());
@@ -34,13 +47,6 @@ $link = mysql_connect("localhost", "mysql_user", "mysql_password")
     while ($row = mysql_fetch_object($db_list)) {
     echo $row->Database . "\n";
     }
-
-
-
-
-
-
-
 
 
 ?>
